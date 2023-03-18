35a20c8ec45629f88bc4acb38c33edfacf6efda8
LeonPuchinger <lepu1590@gmail.com>
2023-03-19 00:14:37 +0100

    create tokens necessary for defining variables

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,3 +1,15 @@
 import { buildLexer } from "typescript-parsec";
 
-export const lexer = buildLexer([]);
+export enum TokenType {
+    whitespace,
+    eq_operator,
+    int_literal,
+    ident,
+}
+
+export const lexer = buildLexer([
+    [false, /^\s+/g, TokenType.whitespace],
+    [true, /^=/g, TokenType.eq_operator],
+    [true, /^[0-9]+/g, TokenType.int_literal],
+    [true, /^[_A-Za-z]+[\-_0-9A-Za-z]*/g, TokenType.ident],
+]);
