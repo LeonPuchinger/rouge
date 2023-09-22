225a7b458661c8dfd8a0063912320f1edddd3bf7
LeonPuchinger <lepu1590@gmail.com>
2023-09-22 17:08:36 +0200

    allow whitespace in variable assignment

--- src/parser.ts
+++ src/parser.ts
@@ -7,12 +7,18 @@ function newRule<TResult>(initializer: (rule: Rule<TokenType, TResult>) => void)
   return empty;
 }
 
+const OPTIONAL_WHITESPACE = newRule((rule) => rule.setPattern(
+  tok(TokenType.whitespace)
+));
+
 const EXPR = rule<TokenType, number>();
 EXPR.setPattern(
   apply(
     seq(
       tok(TokenType.ident),
+      OPTIONAL_WHITESPACE,
       tok(TokenType.eq_operator),
+      OPTIONAL_WHITESPACE,
       tok(TokenType.int_literal),
     ),
     (value) => +value[2], // dummy value: this should evaluate to an AST node
