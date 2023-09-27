2818d62ffce0b2b02fcab305d1d063446817ca54
LeonPuchinger <lepu1590@gmail.com>
2023-09-28 01:47:16 +0200

    remove whitespace in parser rules whitespace is already omitted from the token stream

--- src/parser.ts
+++ src/parser.ts
@@ -1,8 +1,7 @@
-import { apply, rep, seq, tok } from "typescript-parsec";
+import { alt, apply, seq, tok } from "typescript-parsec";
 import { AstNode, AstNodeType } from "./ast.ts";
 import { TokenType } from "./lexer.ts";
 
-const OPTIONAL_WHITESPACE = rep(tok(TokenType.whitespace));
 
 const IDENTIFIER = apply(
   tok(TokenType.ident),
@@ -27,16 +26,14 @@ const INT_LITERAL = apply(
 const ASSIGNMENT = apply(
   seq(
     IDENTIFIER,
-    OPTIONAL_WHITESPACE,
     tok(TokenType.eq_operator),
-    OPTIONAL_WHITESPACE,
     INT_LITERAL,
   ),
   (values) =>
     new AstNode({
       nodeType: AstNodeType.assign,
-      token: values[2],
-      value: values[2].text,
-      children: [values[0], values[4]],
+      token: values[1],
+      value: values[1].text,
+      children: [values[0], values[2]],
     }),
 );
