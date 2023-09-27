71cbf3d3eda5c71ff9da78378bcc76e651a3b9f7
LeonPuchinger <lepu1590@gmail.com>
2023-09-28 01:16:50 +0200

    parse identifier as AST node

--- src/ast.ts
+++ src/ast.ts
@@ -1,14 +1,31 @@
+import { Token } from "typescript-parsec";
+import { TokenType } from "./lexer.ts";
+
 export enum AstNodeType {
   assign,
   ident,
   int_literal,
 }
 
+export interface AstNodeParams {
+  nodeType: AstNodeType;
+  token: Token<TokenType>;
+  value: number | string;
+  children?: Array<AstNode>;
+}
+
 export class AstNode {
-  constructor(
-    public nodeType: AstNodeType,
-    public children: Array<AstNode> = [],
-  ) {}
+  nodeType: AstNodeType;
+  token: Token<TokenType>;
+  value: number | string;
+  children: Array<AstNode>;
+
+  constructor(params: AstNodeParams) {
+    this.nodeType = params.nodeType;
+    this.token = params.token;
+    this.value = params.value;
+    this.children = params.children ?? [];
+  }
 
   addChild(child: AstNode) {
     this.children.push(child);
--- src/parser.ts
+++ src/parser.ts
@@ -4,15 +4,30 @@ import { TokenType } from "./lexer.ts";
 
 const OPTIONAL_WHITESPACE = rep(tok(TokenType.whitespace));
 
+const IDENTIFIER = apply(
+  tok(TokenType.ident),
+  (value) =>
+    new AstNode({
+      nodeType: AstNodeType.ident,
+      token: value,
+      value: value.text,
+    }),
+);
+
 const ASSIGNMENT = apply(
   seq(
-    tok(TokenType.ident),
+    IDENTIFIER,
     OPTIONAL_WHITESPACE,
     tok(TokenType.eq_operator),
     OPTIONAL_WHITESPACE,
     tok(TokenType.int_literal),
   ),
-  (value) => {
-    return new AstNode(AstNodeType.assign, []); // TODO: construct AST node children
-  },
+  (values) =>
+    new AstNode({
+      nodeType: AstNodeType.assign,
+      token: values[2],
+      value: values[2].text,
+      children: [values[0]/* TODO: add ast node for int literal */
+      ],
+    }),
 );
