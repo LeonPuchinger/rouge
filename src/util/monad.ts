f26b0adce118d851756f1bdb0f34c88dcf7f1ab5
Leon Puchinger <lepu1590@gmail.com>
2023-10-04 16:42:52 +0200

    allow using option operations as methods

--- src/util/monad.ts
+++ src/util/monad.ts
@@ -15,3 +15,27 @@ export function map<T, U>(option: Option<T>, fn: (value: T) => U): Option<U> {
 export function unwrapOr<T>(option: Option<T>, defaultValue: T): T {
   return option.kind === 'some' ? option.value : defaultValue;
 }
+
+// alternative implementation
+// operations on Option(al) can be called as methods
+// TODO: get rid of new keyword when instantiating a new Option(al)
+
+interface Optional<T> {
+  map<U>(fn: (value: T) => U): Optional<U>;
+}
+
+export class Some<T> implements Optional<T> {
+  constructor(
+    public value: T,
+  ) {}
+
+  map<U> (fn: (value: T) => U): Optional<U> {
+    return new Some(fn(this.value));
+  }
+}
+
+export class None<T> implements Optional<T> {
+  map<U> (_fn: (value: T) => U): Optional<U> {
+    return new None() as Optional<U>;
+  }
+}
