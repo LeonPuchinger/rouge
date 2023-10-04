dbf1396084a3fc53a1c1be5aab0f6856767f9a00
Leon Puchinger <lepu1590@gmail.com>
2023-10-04 17:02:24 +0200

    allow unwraping option with default value

--- src/util/monad.ts
+++ src/util/monad.ts
@@ -1,24 +1,33 @@
 export interface Option<T> {
   kind: "some" | "none";
   map<U>(fn: (value: T) => U): Option<U>;
+  unwrapOr(defaultValue: T): T;
 }
 
 export function Some<T>(value: T): Option<T> {
   return {
     kind: "some",
 
-    map: function<U> (fn: (value: T) => U): Option<U> {
+    map<U>(fn: (value: T) => U): Option<U> {
       return Some(fn(value));
     },
-  }
+
+    unwrapOr(_defaultValue: T): T {
+      return value;
+    },
+  };
 }
 
 export function None<T>(): Option<T> {
   return {
     kind: "none",
-    
-    map: function<U> (_fn: (value: T) => U): Option<U> {
+
+    map<U>(_fn: (value: T) => U): Option<U> {
       return None() as Option<U>;
     },
-  }
+
+    unwrapOr(defaultValue: T): T {
+      return defaultValue;
+    },
+  };
 }
