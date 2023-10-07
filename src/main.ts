5b04d78e99f7822a070928c38ca4759c29ad4878
LeonPuchinger <lepu1590@gmail.com>
2023-10-07 22:34:43 +0200

    clean up entry point to interpreter

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,19 @@ export function run(source: string) {
   const tokenStream = lexer.parse(source);
   const parseResult = parser.parse(tokenStream);
   if (!parseResult.successful) {
+    // TODO: error handling, logging
     console.log("An error occured while parsing:");
     console.log(parseResult.error);
     return;
   }
   const numberCandidates = parseResult.candidates.length;
   if (numberCandidates > 1) {
+    // TODO: error handling, logging
     console.log("Ambiguity detected");
     console.log(`There are ${numberCandidates} ways to parse the input.`);
-    console.log("In the following, only the first possible AST is shown.");
+    console.log("In the following, the first possible AST is used.");
     console.log("Use the debugger to inspect the other results.");
   }
-  const first_candidate = parseResult.candidates[0];
-  interpret(first_candidate.result);
+  const ast = parseResult.candidates[0].result;
+  interpret(ast);
 }
