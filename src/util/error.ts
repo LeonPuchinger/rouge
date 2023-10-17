7a8be3895f01df2630f4cc68ec41c55e1dc82b4b
LeonPuchinger <lepu1590@gmail.com>
2023-10-18 00:03:13 +0200

    only show stacktrace for locatable errors

--- src/util/error.ts
+++ src/util/error.ts
@@ -3,8 +3,6 @@ export function Panic(reason: string): Error {
 }
 
 export interface LocatableError {
-  file: string;
-  lineno: number;
   stacktrace: string[];
 }
 
@@ -25,34 +23,6 @@ function captureStackTrace(subtractFrames = 0): string[] {
   }
 }
 
-function getCallerFileName(subtractFrames = 0): string {
-  const stackTrace = captureStackTrace(subtractFrames + 1);
-  if (stackTrace.length > 0) {
-    const callerLine = stackTrace[0];
-    const fileNameMatch = callerLine.match(/\((.*):[0-9]+:[0-9]+\)/);
-    if (fileNameMatch && fileNameMatch.length > 1) {
-      return fileNameMatch[1];
-    }
-  }
-  throw Panic(
-    "Could not determine file name while trying to generate an error",
-  );
-}
-
-function getCallerLineNumber(subtractFrames = 0): number {
-  const stackTrace = captureStackTrace(subtractFrames + 1);
-  if (stackTrace.length > 0) {
-    const callerLine = stackTrace[0];
-    const lineNumberMatch = callerLine.match(/\((.*):([0-9]+):[0-9]+\)/);
-    if (lineNumberMatch && lineNumberMatch.length > 2) {
-      return parseInt(lineNumberMatch[2]);
-    }
-  }
-  throw Panic(
-    "Could not determine line number while trying to generate an error",
-  );
-}
-
 function toMultiline(lines: string[]): string {
   return lines.join("\n");
 }
@@ -61,14 +31,10 @@ export function InternalError(
   message: string,
 ): AppError {
   return {
-    file: getCallerFileName(1),
-    lineno: getCallerLineNumber(1),
     stacktrace: captureStackTrace(1),
     toString() {
       return toMultiline([
         `INTERNAL ERROR: ${message}`,
-        `at: ${this.file}:${this.lineno}`,
-        `stacktrace:`,
         `${toMultiline(this.stacktrace)}`,
       ]);
     },
