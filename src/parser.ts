6f7aaaa89b905227c5ff8a3116a652f250b5f426
Leon Puchinger <lepu1590@gmail.com>
2023-10-26 16:01:07 +0200

    add missing parser top level function

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,10 @@
-import { apply, list_sc, seq, tok } from "typescript-parsec";
+import { apply, list_sc, seq, tok, Token } from "typescript-parsec";
 import { AstNode, AstNodeType } from "./ast.ts";
 import { TokenType } from "./lexer.ts";
+import { AppError, InternalError, Panic } from "./util/error.ts";
+import * as logger from "./util/logger.ts";
+import { Err, Ok, Result } from "./util/monad/index.ts";
+import { toMultiline } from "./util/string.ts";
 
 const BREAKING_WHITESPACE = tok(TokenType.breaking_whitespace);
 
@@ -51,4 +55,33 @@ const EXPRESSIONS = apply(
     }),
 );
 
-export const parser = EXPRESSIONS;
+export const START = EXPRESSIONS;
+
+export function parse(
+  tokenStream: Token<TokenType>,
+): Result<AstNode, AppError> {
+  const parseResult = START.parse(tokenStream);
+  if (!parseResult.successful) {
+    const parseError = parseResult.error;
+    return Err(InternalError(toMultiline(
+      "Encountered Syntax Error:",
+      parseError.message,
+    )));
+  }
+  const numberCandidates = parseResult.candidates.length;
+  if (numberCandidates < 1) {
+    throw Panic(
+      "Parsing was successful but the parser did not yield any results",
+    );
+  }
+  if (numberCandidates > 1) {
+    logger.debug(toMultiline(
+      "Ambiguity detected",
+      `There are ${numberCandidates} ways to parse the input.`,
+      "The first possible AST is used.",
+      "Use the debugger to inspect other ways the input can be interpreted by the parser.",
+    ));
+  }
+  const ast = parseResult.candidates[0].result;
+  return Ok(ast);
+}
