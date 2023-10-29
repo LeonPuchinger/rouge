d2c971ab78e66e41d1a54d532ebff4460e1dc96e
Leon Puchinger <lepu1590@gmail.com>
2023-10-29 11:49:04 +0100

    allow type safe read access to environment

--- src/util/environment.ts
+++ src/util/environment.ts
@@ -13,10 +13,23 @@ let environment: Environment = {
  * Update entries in the environment.
  *
  * @param updateEnvironment Entries to override in the current environment.
+ * Existing entries that are not specified here are not overridden.
  */
 export function updateEnvironment(updateEnvironment: Partial<Environment>) {
   environment = {
     ...environment,
     ...updateEnvironment,
   };
+}
+
+/**
+ * A type safe way to access entries in the environment.
+ *
+ * @param key A key that exists in `Environment`.
+ * @returns the value in the environment at the key.
+ */
+export function accessEnvironment<Key extends keyof (Environment)>(
+  key: Key,
+): Environment[Key] {
+  return environment[key];
 }
