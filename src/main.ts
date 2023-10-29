0ada8bc58ee4a207e281f4e38acd578afca722c8
Leon Puchinger <lepu1590@gmail.com>
2023-10-29 12:07:20 +0100

    use env to get input source to error handling

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { interpret } from "./interpreter.ts";
 import { tokenize } from "./lexer.ts";
 import { parse } from "./parser.ts";
+import { updateEnvironment } from "./util/environment.ts";
 import { AppError } from "./util/error.ts";
 import { None, Option, Some } from "./util/monad/index.ts";
 
 export function run(source: string): Option<AppError> {
+  updateEnvironment({ source: source });
   const tokenStream = tokenize(source);
   if (tokenStream.kind === "err") {
     return Some(tokenStream.unwrapError());
