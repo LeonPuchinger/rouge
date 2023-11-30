9f95687c9ffd55b89ec8bb5a967b3d398649d43b
Leon Puchinger <lepu1590@gmail.com>
2023-11-30 17:14:29 +0100

    allow indenting list of strings

--- src/util/string.ts
+++ src/util/string.ts
@@ -5,3 +5,7 @@ export function toMultiline(...lines: string[]): string {
 export function concatLines(...lines: string[]): string {
   return lines.join(" ");
 }
+
+export function indentLines(lines: string[], width: number): string[] {
+  return lines.map((line) => `${" ".repeat(width)}${line}`);
+}
