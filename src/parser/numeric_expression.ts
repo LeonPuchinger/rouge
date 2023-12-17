b651f5e1286a9385b8934ae1196df6aacde05b6d
LeonPuchinger <lepu1590@gmail.com>
2023-12-17 01:07:46 +0100

    move declarations into right order

--- src/parser/numeric_expression.ts
+++ src/parser/numeric_expression.ts
@@ -1,9 +1,10 @@
 import {
   alt_sc,
   apply,
-  expectEOF,
-  list_sc,
-  seq,
+  kmid,
+  nil,
+  Parser,
+  str,
   tok,
 } from "typescript-parsec";
 import * as analysis from "../analysis.ts";
@@ -11,6 +12,15 @@ import * as ast from "../ast.ts";
 import * as interpreter from "../interpreter.ts";
 import { TokenType } from "../lexer.ts";
 import { _ } from "../parser.ts";
+
+// TODO: find a better way to do forward declarations
+export let numericExpression: Parser<TokenType, ast.NumberAstNode> = nil();
+
+const parenthesized: Parser<TokenType, ast.NumberAstNode> = kmid(
+  str("("),
+  numericExpression,
+  str(")"),
+);
 
 const literal = apply(
   tok(TokenType.numeric_literal),
@@ -22,6 +32,11 @@ const literal = apply(
     },
     analyze() {
       return analysis.analyzeNumber(this);
-    }
+    },
   }),
+);
+
+numericExpression = alt_sc(
+  parenthesized,
+  literal,
 );
