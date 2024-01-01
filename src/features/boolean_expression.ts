22bdc578c45d20d65471a316abd4061f4e21888c
LeonPuchinger <lepu1590@gmail.com>
2024-01-01 19:07:16 +0100

    handle binary boolean expressions

--- src/features/boolean_expression.ts
+++ src/features/boolean_expression.ts
@@ -3,8 +3,10 @@ import {
   apply,
   kmid,
   kright,
+  lrec_sc,
   Parser,
   rule,
+  seq,
   str,
   tok,
   Token,
@@ -13,8 +15,8 @@ import { AnalysisResult } from "../analysis.ts";
 import * as ast from "../ast.ts";
 import { TokenType } from "../lexer.ts";
 import { SymbolValue, SymbolValueKind } from "../symbol.ts";
-import { AppError } from "../util/error.ts";
-import { Ok, Result, Some } from "../util/monad/index.ts";
+import { AppError, InternalError } from "../util/error.ts";
+import { Err, Ok, Result, Some } from "../util/monad/index.ts";
 
 /* AST NODES */
 
@@ -98,6 +100,81 @@ function evaluateBooleanNegationAstNode(
     );
 }
 
+/* Binary Boolean Expression */
+
+type BinaryBooleanExpressionAstNode =
+  & ast.BinaryAstNode<BooleanExpressionAstNode, BooleanExpressionAstNode>
+  & ast.TokenAstNode
+  & BooleanExpressionAstNode;
+
+function createBinaryBooleanExpressionAstNode(params: {
+  lhs: BooleanExpressionAstNode;
+  rhs: BooleanExpressionAstNode;
+  token: Token<TokenType>;
+}): BinaryBooleanExpressionAstNode {
+  return {
+    ...params,
+    analyze() {
+      return analyzeBinaryExpression();
+    },
+    evaluate() {
+      return evaluateBinaryExpression(this);
+    },
+  };
+}
+
+function analyzeBinaryExpression() {
+  return {
+    value: Some(SymbolValueKind.boolean),
+    warnings: [],
+    errors: [],
+  };
+}
+
+function evaluateBinaryExpression(
+  node: BinaryBooleanExpressionAstNode,
+): Result<SymbolValue<boolean>, AppError> {
+  if (
+    !["==", "!=", ">", ">=", "<", "<=", "&&", "||", "^"]
+      .includes(node.token.text)
+  ) {
+    return Err(InternalError(
+      `The interpreter recieved instructions to perform the following unknown operation on two booleans: ${node.token.text}`,
+      "This should have either been caught during static analysis or be prevented by the parser.",
+    ));
+  }
+  return node.lhs.evaluate()
+    .combine(node.rhs.evaluate())
+    .map(([left, right]) => {
+      switch (node.token.text) {
+        case "==":
+          return left.value == right.value;
+        case "!=":
+          return left.value != right.value;
+        case ">":
+          return left.value > right.value;
+        case ">=":
+          return left.value >= right.value;
+        case "<":
+          return left.value < right.value;
+        case "<=":
+          return left.value <= right.value;
+        case "&&":
+          return left.value && right.value;
+        case "||":
+          return left.value || right.value;
+        case "^":
+          return (left.value || right.value) && !(left.value && right.value);
+        default:
+          // this never happens, TS simply does not get that the symbol of operations has been checked previously.
+          return false;
+      }
+    })
+    .map((result) =>
+      new SymbolValue({ value: result, valueKind: SymbolValueKind.number })
+    );
+}
+
 /* Boolean Expression */
 
 type BooleanExpressionAstNode = ast.EvaluableAstNode<SymbolValue<boolean>>;
@@ -137,4 +214,24 @@ const booleanOperand: Parser<TokenType, BooleanExpressionAstNode> = alt_sc(
   negation,
   parenthesized,
   literal,
+);
+
+const binaryBooleanExpression = alt_sc(
+  lrec_sc(
+    booleanOperand,
+    seq(
+      alt_sc(str("+"), str("-")),
+      booleanOperand,
+    ),
+    (
+      a: BooleanExpressionAstNode,
+      b: [Token<TokenType>, BooleanExpressionAstNode],
+    ): BooleanExpressionAstNode =>
+      createBinaryBooleanExpressionAstNode({
+        lhs: a,
+        token: b[0],
+        rhs: b[1],
+      }),
+  ),
+  booleanOperand,
 );
