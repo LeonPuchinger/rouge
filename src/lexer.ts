5f6d0afd6f31a07eda61873da98b976f7d0aec60
LeonPuchinger <lepu1590@gmail.com>
2024-01-02 12:07:23 +0100

    tokenize punctuation

--- src/lexer.ts
+++ src/lexer.ts
@@ -8,16 +8,18 @@ export enum TokenType {
   numeric_literal,
   boolean_literal,
   ident,
+  punctuation,
   unspecified,
 }
 
 export const lexer = buildLexer([
+  [true, /^\s*\n\s*/g, TokenType.breaking_whitespace],
+  [false, /^\s+/g, TokenType.whitespace],
   [true, /^[0-9]+(\.[0-9]+)?/g, TokenType.numeric_literal],
   [true, /^(false|true)/g, TokenType.boolean_literal],
   [true, /^[_A-Za-z]+[\-_0-9A-Za-z]*/g, TokenType.ident],
-  [true, /^\S+/g, TokenType.unspecified],
-  [true, /^\s*\n\s*/g, TokenType.breaking_whitespace],
-  [false, /^\s+/g, TokenType.whitespace],
+  [true, /^[!@=#$%^&*()_+{}\[\]:;<>\|,.?~\\/\-]+/g, TokenType.punctuation],
+  [true, /^\S/g, TokenType.unspecified],
 ]);
 
 /**
