ec82f2ec90de4c2dbb2c30d8b3840731996dceae
LeonPuchinger <lepu1590@gmail.com>
2024-02-03 23:17:11 +0100

    a symbol's type is only saved with the symbol value

--- src/symbol.ts
+++ src/symbol.ts
@@ -1,45 +1,34 @@
 import { AstNode } from "./ast.ts";
-import { Panic } from "./util/error.ts";
 import { None, Option, Some } from "./util/monad/index.ts";
 
 // Symbol
 
-export enum SymbolKind {
-  variable,
-}
-
 interface SymbolParams {
-  symbolKind: SymbolKind;
   node?: AstNode;
   value: SymbolValue<unknown>;
 }
 
 interface Symbol {
-  symbolKind: SymbolKind;
   node: Option<AstNode>;
 }
 
 export class RuntimeSymbol implements Symbol {
-  symbolKind: SymbolKind;
   node: Option<AstNode>;
   value: SymbolValue<unknown>;
 
   constructor(params: SymbolParams) {
-    this.symbolKind = params.symbolKind;
     this.node = Some(params.node);
     this.value = params.value;
   }
 }
 
 export class StaticSymbol implements Symbol {
-  symbolKind: SymbolKind;
   node: Option<AstNode>;
   valueKind: SymbolValueKind;
 
   constructor(
     params: Omit<SymbolParams, "value"> & { valueKind: SymbolValueKind },
   ) {
-    this.symbolKind = params.symbolKind;
     this.node = Some(params.node);
     this.valueKind = params.valueKind;
   }
@@ -64,15 +53,6 @@ export class SymbolValue<T> {
   constructor(params: SymbolValueParams<T>) {
     this.valueKind = params.valueKind;
     this.value = params.value;
-  }
-
-  asNumber(): SymbolValue<number> {
-    if (this.valueKind !== SymbolValueKind.number) {
-      throw Panic(
-        "tried to access the value of a non-numeric symbol as a number",
-      );
-    }
-    return this as SymbolValue<number>;
   }
 }
 
@@ -100,32 +80,24 @@ export class SymbolTable<SymbolType extends Symbol> {
   private findSymbolInScope(
     name: string,
     scope: Scope<SymbolType>,
-    symbolKind?: SymbolKind,
   ): Option<SymbolType> {
     const symbol = scope.get(name);
-    if (symbolKind === undefined) {
-      return Some(symbol);
-    }
-    if (symbol?.symbolKind === symbolKind) {
-      return Some(symbol);
-    }
-    return None();
+    return Some(symbol);
   }
 
   findSymbolInCurrentScope(
     name: string,
-    symbolKind?: SymbolKind,
   ): Option<SymbolType> {
     const current = this.scopes.toReversed().at(0);
     if (current !== undefined) {
-      return this.findSymbolInScope(name, current, symbolKind);
+      return this.findSymbolInScope(name, current);
     }
     return None();
   }
 
-  findSymbol(name: string, symbolKind?: SymbolKind): Option<SymbolType> {
+  findSymbol(name: string): Option<SymbolType> {
     for (const current_scope of this.scopes.toReversed()) {
-      const symbol = this.findSymbolInScope(name, current_scope, symbolKind);
+      const symbol = this.findSymbolInScope(name, current_scope);
       if (symbol.kind === "none") {
         continue;
       }
