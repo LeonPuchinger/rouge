2c4bd1e86f5dafdd9cd9956d300390240d4d805d
LeonPuchinger <lepu1590@gmail.com>
2024-02-27 22:58:15 +0100

    add notice to introduce return statements

--- src/features/function.ts
+++ src/features/function.ts
@@ -19,11 +19,10 @@ import { EvaluableAstNode } from "../ast.ts";
 import { AnalysisError } from "../finding.ts";
 import { TokenType } from "../lexer.ts";
 import { statements } from "../parser.ts";
-import { resolveType, SymbolType, SymbolValue } from "../symbol.ts";
+import { FunctionSymbolType, resolveType, SymbolType, SymbolValue } from "../symbol.ts";
 import { AppError } from "../util/error.ts";
 import { emptyFindings, mergeFindings } from "../util/finding.ts";
-import { None, Some } from "../util/monad/index.ts";
-import { Result } from "../util/monad/result.ts";
+import { None, Option, Result, Some } from "../util/monad/index.ts";
 import { kouter } from "../util/parser.ts";
 import { Attributes } from "../util/type.ts";
 
@@ -70,6 +69,7 @@ class ParameterAstNode {
 
 class FunctionAstNode implements EvaluableAstNode {
   parameters!: ParameterAstNode[];
+  returnType!: Option<Token<TokenType>>;
 
   constructor(params: Attributes<FunctionAstNode>) {
     Object.assign(this, params);
@@ -84,6 +84,20 @@ class FunctionAstNode implements EvaluableAstNode {
     this.parameters.map((parameter) =>
       mergeFindings(findings, parameter.check())
     );
+    const parameterTypes = this.parameters.map((parameter) =>
+      parameter.resolveType()
+    );
+    const returnType = this.returnType.map((token) => resolveType(token.text));
+    // TODO: introduce return statements and check with return type
+    return {
+      ...findings,
+      value: Some(
+        new FunctionSymbolType({
+          parameters: parameterTypes,
+          returnType: returnType,
+        }),
+      ),
+    };
   }
 }
 
@@ -112,7 +126,7 @@ const parameters = apply(
 
 const returnType = apply(
   opt_sc(tok(TokenType.ident)),
-  (token) => Some(token).map((token) => token.text),
+  (token) => Some(token),
 );
 
 export const functionDefinition = apply(
@@ -135,8 +149,9 @@ export const functionDefinition = apply(
       ),
     ),
   ),
-  ([parameters, _returnType, _statements]) =>
+  ([parameters, returnType, _statements]) =>
     new FunctionAstNode({
       parameters: parameters,
+      returnType: returnType,
     }),
 );
