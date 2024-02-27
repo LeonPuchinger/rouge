3adb1b99cc6d936d24bc67238c29bb1f6db45e28
LeonPuchinger <lepu1590@gmail.com>
2024-02-27 21:52:00 +0100

    allow properties of dataclasses that implement `Option` to be truly optional in the constructor

--- src/util/type.ts
+++ src/util/type.ts
@@ -1,3 +1,5 @@
+import { Option } from "./monad/index.ts";
+
 /**
  * Omits all properties of a type that are functions.
  */
@@ -7,6 +9,32 @@ type OmitFunctions<T> = {
 };
 
 /**
+ * Omits all properties of a type that implement `Option`.
+ */
+type OmitOptions<T> = {
+  [K in keyof T as T[K] extends Option<unknown> ? never : K]: T[K];
+};
+
+/**
+ * Omits all properties of a type that *don't* implement `Option`.
+ */
+type OnlyOptions<T> = {
+  [K in keyof T as T[K] extends Option<unknown> ? K : never]: T[K];
+};
+
+/**
+ * Convert all properties of a type that implement `Option<T>`
+ * to be of type `T` and mark them as truly optional.
+ */
+type ConvertOptionals<T> =
+  & {
+    [K in keyof OnlyOptions<T>]?: T[K] extends Option<infer S> ? S : never;
+  }
+  & OmitOptions<T>;
+
+/**
  * Generate a type from all public members of a class (excluding methods).
  */
 export type Attributes<T> = OmitFunctions<T>;
+
+export type WithOptionalAttributes<T> = ConvertOptionals<Attributes<T>>;
