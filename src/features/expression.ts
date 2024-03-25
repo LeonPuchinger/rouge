640163345348ca981b5cafa67e29cb0cb5cdd58e
LeonPuchinger <lepu1590@gmail.com>
2024-03-25 15:24:44 +0100

    move symbol expression to dataclass format

--- src/features/expression.ts
+++ src/features/expression.ts
@@ -1,73 +1,66 @@
-import { apply, tok } from "typescript-parsec";
+import { apply, tok, Token } from "typescript-parsec";
 import { analysisTable } from "../analysis.ts";
 import { EvaluableAstNode, TokenAstNode } from "../ast.ts";
 import { AnalysisError, AnalysisFindings } from "../finding.ts";
 import { runtimeTable } from "../interpreter.ts";
 import { TokenType } from "../lexer.ts";
-import { SymbolValue } from "../symbol.ts";
+import { SymbolType, SymbolValue } from "../symbol.ts";
 import { InternalError } from "../util/error.ts";
 import { None } from "../util/monad/index.ts";
 
 /* Identifier expression */
 
-export type SymbolExpressionAstNode =
-  & TokenAstNode
-  & EvaluableAstNode<SymbolValue<unknown>>;
+export class SymbolExpressionAstNode implements TokenAstNode, EvaluableAstNode {
+  token: Token<TokenType>;
 
-export function evaluateSymbolExpression(
-  node: SymbolExpressionAstNode,
-): SymbolValue<unknown> {
-  const ident = node.token.text;
-  return runtimeTable
-    .findSymbol(ident)
-    .map((symbol) => symbol.value)
-    .unwrapOrThrow(
-      new InternalError(
-        `Unable to resolve symbol ${ident} in the symbol table.`,
-        "This should have been caught during static analysis.",
-      ),
-    );
-}
+  constructor(identifier: Token<TokenType>) {
+    this.token = identifier;
+  }
 
-export function analyzeSymbolExpression(
-  node: SymbolExpressionAstNode,
-): AnalysisFindings {
-  const ident = node.token.text;
-  const findings = AnalysisFindings.empty();
-  analysisTable.findSymbol(ident).onNone(() => {
-    findings.errors.push(
-      AnalysisError({
-        message:
-          "You tried to use a variable that has not been defined at this point in the program.",
-        beginHighlight: node,
-        endHighlight: None(),
-        messageHighlight: `Variable "${ident}" is unknown at this point.`,
-      }),
-    );
-  });
-  return findings;
+  evaluate(): SymbolValue<unknown> {
+    const ident = this.token.text;
+    return runtimeTable
+      .findSymbol(ident)
+      .map((symbol) => symbol.value)
+      .unwrapOrThrow(
+        new InternalError(
+          `Unable to resolve symbol ${ident} in the symbol table.`,
+          "This should have been caught during static analysis.",
+        ),
+      );
+  }
+
+  analyze(): AnalysisFindings {
+    const ident = this.token.text;
+    const findings = AnalysisFindings.empty();
+    analysisTable.findSymbol(ident).onNone(() => {
+      findings.errors.push(
+        AnalysisError({
+          message:
+            "You tried to use a variable that has not been defined at this point in the program.",
+          beginHighlight: this,
+          endHighlight: None(),
+          messageHighlight: `Variable "${ident}" is unknown at this point.`,
+        }),
+      );
+    });
+    return findings;
+  }
+
+  resolveType(): SymbolType {
+    return analysisTable
+      .findSymbol(this.token.text)
+      .map((symbol) => symbol.valueKind)
+      .unwrapOrThrow(
+        new InternalError(
+          "Unable to resolve a symbol in the symbol table.",
+          "This should have been caught by static analysis.",
+        ),
+      );
+  }
 }
 
 export const symbolExpression = apply(
   tok(TokenType.ident),
-  (identifier): SymbolExpressionAstNode => ({
-    token: identifier,
-    evaluate() {
-      return evaluateSymbolExpression(this);
-    },
-    analyze() {
-      return analyzeSymbolExpression(this);
-    },
-    resolveType() {
-      return analysisTable
-        .findSymbol(this.token.text)
-        .map((symbol) => symbol.valueKind)
-        .unwrapOrThrow(
-          new InternalError(
-            "Unable to resolve a symbol in the symbol table.",
-            "This should have been caught by static analysis.",
-          ),
-        );
-    },
-  }),
+  (identifier) => new SymbolExpressionAstNode(identifier),
 );
