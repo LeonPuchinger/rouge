35541642f41e5ca371525743d4ed215bd1f01749
LeonPuchinger <lepu1590@gmail.com>
2024-04-10 16:02:22 +0200

    create more readable way to calculate token range for list of statements

--- src/features/statement.ts
+++ src/features/statement.ts
@@ -2,10 +2,12 @@ import { alt_sc, apply, list_sc, tok, Token } from "typescript-parsec";
 import { InterpretableAstNode, NaryAstNode } from "../ast.ts";
 import { AnalysisFindings } from "../finding.ts";
 import { TokenKind } from "../lexer.ts";
+import { InternalError } from "../util/error.ts";
 import { Attributes } from "../util/type.ts";
 import { assignment, AssignmentAstNode } from "./assignment.ts";
 import { expression, ExpressionAstNode } from "./expression.ts";
-import { InternalError } from "../util/error.ts";
+// required for extension methods to be usable
+import { } from "../util/array.ts";
 
 /* AST NODES */
 
@@ -42,7 +44,7 @@ export class StatementsAstNode
     }
     return [
       this.children[0].tokenRange()[0],
-      this.children.slice(-1)[0].tokenRange()[1],
+      this.children.last().tokenRange()[1],
     ];
   }
 }
