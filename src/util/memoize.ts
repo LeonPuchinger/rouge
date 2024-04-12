64192c124e41c2794298686550a249aff5a3559d
LeonPuchinger <lepu1590@gmail.com>
2024-04-12 16:58:16 +0200

    document memoize decorator

--- src/util/memoize.ts
+++ src/util/memoize.ts
@@ -1,6 +1,25 @@
 // deno-lint-ignore-file no-explicit-any
 import { None, Option, Some } from "./monad/index.ts";
 
+/**
+ * A decorator used for caching return values of methods.
+ * The decorator works lazily which means the result is calculated the first time the method is called.
+ * All subsequent calls to the method return the cached result from the first call.
+ * For instance, this decorator can be used to cache the return values when evaluating AST nodes (see example below).
+ *
+ * Example:
+ *
+ * class MyAstNode implements EvaluableAstNode {
+ *
+ *  @memoize
+ *  evaluate() {
+ *    return expensiveComputation();
+ *  }
+ *
+ *  // remaining methods
+ *
+ * }
+ */
 export function memoize(
   _target: any,
   _key: string,
