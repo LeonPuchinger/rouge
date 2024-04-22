77c1c6689252a868ddf6879ef007f2f5133fcc72
LeonPuchinger <lepu1590@gmail.com>
2024-04-22 14:13:02 +0200

    document safe usage of type resolving

--- src/ast.ts
+++ src/ast.ts
@@ -37,6 +37,11 @@ export interface EvaluableAstNode<R = SymbolValue<unknown>> extends AstNode {
 
   /**
    * Returns the corresponding `SymbolType` of the evaluated result.
+   * Before this method can be called, it has to be made sure
+   * that `analyze` is called first on the AST node and its result is inspected.
+   * Static analysis should catch any errors in regards to type resolving (e.g. the type does not exist).
+   * Even though implementations of `resolveType` can assume that `analyze` has been called already,
+   * an `InternalError` should still be thrown in case `analyze` has not been called and type resolving fails.
    */
   resolveType(): SymbolType;
 }
