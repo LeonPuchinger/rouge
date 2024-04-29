6af07a02e80d7fa2ae0e9d38dab65de67a85ae03
LeonPuchinger <lepu1590@gmail.com>
2024-04-29 16:29:14 +0200

    interpret else branch of the condition

--- src/features/condition.ts
+++ src/features/condition.ts
@@ -2,14 +2,17 @@ import { apply, kmid, kright, opt, seq, str, Token } from "typescript-parsec";
 import { InterpretableAstNode } from "../ast.ts";
 import { AnalysisError, AnalysisFindings } from "../finding.ts";
 import { TokenKind } from "../lexer.ts";
-import { None } from "../util/monad/option.ts";
-import { surround_with_breaking_whitespace } from "../util/parser.ts";
-import { Attributes } from "../util/type.ts";
+import { None, Option } from "../util/monad/index.ts";
+import { Some } from "../util/monad/option.ts";
+import {
+  ends_with_breaking_whitespace,
+  starts_with_breaking_whitespace,
+  surround_with_breaking_whitespace,
+} from "../util/parser.ts";
+import { WithOptionalAttributes } from "../util/type.ts";
 import { BooleanExpressionAstNode } from "./boolean_expression.ts";
 import { expression, ExpressionAstNode } from "./expression.ts";
 import { statements, StatementsAstNode } from "./statement.ts";
-import { starts_with_breaking_whitespace } from "../util/parser.ts";
-import { ends_with_breaking_whitespace } from "../util/parser.ts";
 
 /* AST NODES */
 
@@ -17,10 +20,14 @@ class ConditionAstNode implements InterpretableAstNode {
   ifKeyword!: Token<TokenKind>;
   condition!: ExpressionAstNode;
   trueStatements!: StatementsAstNode;
-  closingBrace!: Token<TokenKind>;
+  falseStatements!: Option<StatementsAstNode>;
+  ifClosingBrace!: Token<TokenKind>;
+  elseClosingBrace!: Option<Token<TokenKind>>;
 
-  constructor(params: Attributes<ConditionAstNode>) {
+  constructor(params: WithOptionalAttributes<ConditionAstNode>) {
     Object.assign(this, params);
+    this.falseStatements = Some(params.falseStatements);
+    this.elseClosingBrace = Some(params.elseClosingBrace);
   }
 
   analyze(): AnalysisFindings {
@@ -28,6 +35,9 @@ class ConditionAstNode implements InterpretableAstNode {
     const findings = AnalysisFindings.merge(
       conditionFindings,
       this.trueStatements.analyze(),
+      this.falseStatements
+        .map((statements) => statements.analyze())
+        .unwrapOr(AnalysisFindings.empty()),
     );
     if (conditionFindings.isErroneous()) {
       return findings;
@@ -49,11 +59,16 @@ class ConditionAstNode implements InterpretableAstNode {
       .evaluate();
     if (conditionResult.value) {
       this.trueStatements.interpret();
+    } else {
+      this.falseStatements.then((statements) => statements.interpret());
     }
   }
 
   tokenRange(): [Token<TokenKind>, Token<TokenKind>] {
-    return [this.ifKeyword, this.closingBrace];
+    return [
+      this.ifKeyword,
+      this.elseClosingBrace.unwrapOr(this.ifClosingBrace),
+    ];
   }
 }
 
@@ -85,11 +100,15 @@ export const condition = apply(
     surround_with_breaking_whitespace(str("}")),
     opt(elseBranch),
   ),
-  ([ifKeyword, condition, trueStatements, closingBrace]) =>
-    new ConditionAstNode({
+  ([ifKeyword, condition, ifStatements, firstClosingBrace, elseBranch]) => {
+    const [falseStatements, elseClosingBrace] = elseBranch ?? [];
+    return new ConditionAstNode({
       ifKeyword: ifKeyword,
       condition: condition,
-      trueStatements: trueStatements,
-      closingBrace: closingBrace,
-    }),
+      trueStatements: ifStatements,
+      ifClosingBrace: firstClosingBrace,
+      falseStatements: falseStatements,
+      elseClosingBrace: elseClosingBrace,
+    });
+  },
 );
