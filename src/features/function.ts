c86ddd7f9a3e252f885182d41ef979f55b3f3663
LeonPuchinger <lepu1590@gmail.com>
2024-05-24 13:34:46 +0200

    remove setting flags when those are no longer used

--- src/features/function.ts
+++ src/features/function.ts
@@ -171,7 +171,6 @@ export class FunctionAstNode implements EvaluableAstNode {
 
   constructor(params: Attributes<FunctionAstNode>) {
     Object.assign(this, params);
-    this.statements.configure({ representsFrame: true });
   }
 
   evaluate(): SymbolValue<Function> {
