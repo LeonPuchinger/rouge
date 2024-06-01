b7dd797b19b0f0b5723515bdfe68176ae64b184f
Leon Puchinger <lepu1590@gmail.com>
2024-06-02 00:09:05 +0200

    analyze type annotations

--- src/features/assignment.ts
+++ src/features/assignment.ts
@@ -1,4 +1,4 @@
-import { apply, kright, seq, str, tok, Token } from "typescript-parsec";
+import { apply, kright, opt, seq, str, tok, Token } from "typescript-parsec";
 import { InterpretableAstNode } from "../ast.ts";
 import { AnalysisError, AnalysisFindings } from "../finding.ts";
 import { TokenKind } from "../lexer.ts";
@@ -8,24 +8,27 @@ import {
   runtimeTable,
   StaticSymbol,
 } from "../symbol.ts";
-import { None } from "../util/monad/index.ts";
+import { typeTable } from "../type.ts";
+import { None, Option, Some } from "../util/monad/index.ts";
 import {
   ends_with_breaking_whitespace,
   surround_with_breaking_whitespace,
 } from "../util/parser.ts";
+import { DummyAstNode } from "../util/snippet.ts";
 import { concatLines } from "../util/string.ts";
-import { Attributes } from "../util/type.ts";
+import { WithOptionalAttributes } from "../util/type.ts";
 import { expression, ExpressionAstNode } from "./expression.ts";
 
 /* AST NODES */
 
 export class AssignmentAstNode implements InterpretableAstNode {
   token!: Token<TokenKind>;
-  typeAnnotation!: Token<TokenKind>;
+  typeAnnotation!: Option<Token<TokenKind>>;
   child!: ExpressionAstNode;
 
-  constructor(params: Attributes<AssignmentAstNode>) {
+  constructor(params: WithOptionalAttributes<AssignmentAstNode>) {
     Object.assign(this, params);
+    this.typeAnnotation = Some(params.typeAnnotation);
   }
 
   analyze(): AnalysisFindings {
@@ -35,6 +38,31 @@ export class AssignmentAstNode implements InterpretableAstNode {
     }
     const ident = this.token.text;
     const expressionType = this.child.resolveType();
+    this.typeAnnotation.then((annotationName) => {
+      if (!typeTable.typeResolvable(annotationName.text)) {
+        findings.errors.push(AnalysisError({
+          message:
+            "The type that was explicitly annotated in the assignment could not be found.",
+          beginHighlight: DummyAstNode.fromToken(annotationName),
+          endHighlight: None(),
+          messageHighlight: `Type "${annotationName.text}" could not be found.`,
+        }));
+        return;
+      }
+      const resolvedAnnotation = typeTable.findType(annotationName.text)
+        .unwrap();
+      if (!resolvedAnnotation.typeCompatibleWith(expressionType)) {
+        findings.errors.push(AnalysisError({
+          message:
+            "The type that was explicitly annotated in the assignment is not compatible with the type of the assigned value.",
+          beginHighlight: DummyAstNode.fromToken(annotationName),
+          endHighlight: None(),
+          // TODO: allow resolving the names of SymbolTypes (e.g. for this error message)
+          messageHighlight:
+            `Type "${annotationName.text}" is incompatible with the type of the value on the right side of the assignment.`,
+        }));
+      }
+    });
     analysisTable.findSymbol(ident)
       .then((existing) => {
         if (existing.valueType.typeCompatibleWith(expressionType)) {
@@ -90,7 +118,7 @@ const typeAnnotation = kright(
 export const assignment = apply(
   seq(
     tok(TokenKind.ident),
-    surround_with_breaking_whitespace(typeAnnotation),
+    surround_with_breaking_whitespace(opt(typeAnnotation)),
     ends_with_breaking_whitespace(str("=")),
     expression,
   ),
