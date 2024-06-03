93aba0473cb3fcddff02c457f8f566d036dc6219
Leon Puchinger <lepu1590@gmail.com>
2024-06-03 23:09:58 +0200

    parse string literals

---
+++ src/features/string.ts
@@ -0,0 +1,37 @@
+import { apply, tok, Token } from "typescript-parsec";
+import { EvaluableAstNode } from "../ast.ts";
+import { AnalysisFindings } from "../finding.ts";
+import { TokenKind } from "../lexer.ts";
+import { SymbolValue } from "../symbol.ts";
+import { SymbolType } from "../type.ts";
+import { Attributes } from "../util/type.ts";
+
+/* AST NODES */
+
+class StringAstNode implements EvaluableAstNode {
+  token!: Token<TokenKind>;
+
+  constructor(params: Attributes<StringAstNode>) {
+    Object.assign(this, params);
+  }
+
+  evaluate(): SymbolValue<unknown> {
+    throw new Error("Method not implemented.");
+  }
+  resolveType(): SymbolType {
+    throw new Error("Method not implemented.");
+  }
+  analyze(): AnalysisFindings {
+    throw new Error("Method not implemented.");
+  }
+  tokenRange(): [Token<TokenKind>, Token<TokenKind>] {
+    throw new Error("Method not implemented.");
+  }
+}
+
+/* PARSER */
+
+export const stringLiteral = apply(
+  tok(TokenKind.string),
+  (token) => new StringAstNode({ token: token }),
+);
