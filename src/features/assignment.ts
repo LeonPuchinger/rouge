0832bc804cad0139976752b3b2a4ae8f268471ae
LeonPuchinger <lepu1590@gmail.com>
2024-06-14 17:03:29 +0200

    don't resolve assignment type when analysis of expression fails

--- src/features/assignment.ts
+++ src/features/assignment.ts
@@ -36,7 +36,6 @@ export class AssignmentAstNode implements InterpretableAstNode {
     const ident = this.token.text;
     const isInitialAssignment = !analysisTable.findSymbol(ident).hasValue();
     const expressionFindingsErroneous = findings.isErroneous();
-    const expressionType = this.child.resolveType();
     if (isInitialAssignment) {
       this.typeAnnotation.then((annotationName) => {
         if (!typeTable.typeResolvable(annotationName.text)) {
@@ -53,6 +52,7 @@ export class AssignmentAstNode implements InterpretableAstNode {
       if (findings.isErroneous()) {
         return findings;
       }
+      const expressionType = this.child.resolveType();
       this.typeAnnotation.then((annotationName) => {
         const resolvedAnnotation = typeTable.findType(annotationName.text)
           .unwrap();
@@ -86,6 +86,7 @@ export class AssignmentAstNode implements InterpretableAstNode {
       if (expressionFindingsErroneous) {
         return findings;
       }
+      const expressionType = this.child.resolveType();
       analysisTable.findSymbol(ident)
         .then((existing) => {
           if (existing.valueType.typeCompatibleWith(expressionType)) {
