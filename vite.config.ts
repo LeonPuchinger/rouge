7b8a85747e5d9d13b28400d1ed0f719f273949d6
LeonPuchinger <lepu1590@gmail.com>
2024-11-29 15:26:56 +0100

    generate type definitions on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
     build: {
-        target: 'esnext',
-        outDir: 'dist',
+        target: "esnext",
+        outDir: "dist",
         lib: {
-            entry: 'src/main.ts',
-            name: 'Rouge',
-            fileName: format => `rouge.${format}.js`,
+            entry: "src/main.ts",
+            name: "Rouge",
+            fileName: (format) => `rouge.${format}.js`,
         },
-    }
+    },
+    plugins: [
+        dts({
+            rollupTypes: true,
+        }),
+    ],
 });
