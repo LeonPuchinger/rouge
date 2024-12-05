fe3b3ec4aafa54d8013ceb6227a3a611a0b226dd
Leon Puchinger <24792398+LeonPuchinger@users.noreply.github.com>
2024-12-05 22:41:35 +0100

    Publish package publicly to NPM (#38)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from "vite";
+import vite_dts from "vite-plugin-dts";
 
 export default defineConfig({
     build: {
-        target: 'esnext',
-        outDir: 'dist',
+        target: "esnext",
+        outDir: "dist",
         lib: {
-            entry: 'src/main.ts',
-            name: 'Rouge',
-            fileName: format => `rouge.${format}.js`,
+            entry: "src/main.ts",
+            formats: ["es"],
+            name: "Rouge",
+            fileName: (format) => `index.${format}.js`,
         },
-    }
+    },
+    plugins: [
+        vite_dts({
+            rollupTypes: true,
+            exclude: ["src/cli.ts"],
+        }),
+    ],
 });
