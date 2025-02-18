0bc464fd8af170260b989ad8127f2a028b463c99
Leon Puchinger <lepu1590@gmail.com>
2025-02-18 12:02:48 +0100

    define interface for producing async streams of text

--- src/streams.ts
+++ src/streams.ts
@@ -1,8 +1,14 @@
-interface ReadableStream<T> {
+export interface ReadableStream<T> {
     onNewLine(fn: (line: T) => void): void;
     readLine(): Promise<T>;
     onNewChunk(fn: (chunk: T) => void): void;
     readChunk(): Promise<T>;
     onClose(fn: () => void): void;
     [Symbol.asyncIterator](): AsyncIterator<T>;
+}
+
+export interface WritableStream<T> {
+    writeLine(line: T): void;
+    writeChunk(chunk: T): void;
+    close(): void;
 }
