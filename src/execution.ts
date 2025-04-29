1db652f16c4c3371176398555a5ee71e20e0286d
Leon Puchinger <lepu1590@gmail.com>
2025-04-29 17:01:53 +0200

    keep execution-critical state in the new environment

--- src/execution.ts
+++ src/execution.ts
@@ -1,3 +1,14 @@
-export class ExecutionEnvironment {
+import { AnalysisSymbolTable, InterpreterSymbolTable } from "./symbol.ts";
+import { TypeTable } from "./type.ts";
+import { Attributes } from "./util/type.ts";
 
+export class ExecutionEnvironment {
+  analysisTable!: AnalysisSymbolTable;
+  runtimeTable!: InterpreterSymbolTable;
+  typeTable!: TypeTable;
+  source!: string;
+
+  constructor(params: Attributes<ExecutionEnvironment>) {
+    Object.assign(this, params);
+  }
 }
