9ed6ebff4a614e756b22ad2cfaeaa4871bd36774
LeonPuchinger <lepu1590@gmail.com>
2025-05-08 11:21:16 +0200

    base loop implementation off of conditions

---
+++ src/features/loop.ts
@@ -0,0 +1,118 @@
+import {
+    apply,
+    kmid,
+    kright,
+    seq,
+    str,
+    Token
+} from "typescript-parsec";
+import { InterpretableAstNode } from "../ast.ts";
+import { ExecutionEnvironment } from "../execution.ts";
+import { AnalysisError, AnalysisFindings } from "../finding.ts";
+import { TokenKind } from "../lexer.ts";
+import { None } from "../util/monad/index.ts";
+import {
+    ends_with_breaking_whitespace,
+    surround_with_breaking_whitespace
+} from "../util/parser.ts";
+import { WithOptionalAttributes } from "../util/type.ts";
+import { BooleanExpressionAstNode } from "./boolean_expression.ts";
+import { expression, ExpressionAstNode } from "./expression.ts";
+import { loop, statements } from "./parser_declarations.ts";
+import { StatementsAstNode } from "./statement.ts";
+
+/* AST NODES */
+
+export class LoopAstNode implements InterpretableAstNode {
+  keyword!: Token<TokenKind>;
+  condition!: ExpressionAstNode;
+  statements!: StatementsAstNode;
+  closingBrace!: Token<TokenKind>;
+
+  constructor(params: WithOptionalAttributes<LoopAstNode>) {
+    Object.assign(this, params);
+  }
+
+  analyze(environment: ExecutionEnvironment): AnalysisFindings {
+    environment.analysisTable.pushScope();
+    const conditionFindings = this.condition.analyze(environment);
+    const trueFindings = this.statements.analyze(environment);
+    environment.analysisTable.popScope();
+    environment.analysisTable.pushScope();
+    const findings = AnalysisFindings.merge(
+      conditionFindings,
+      trueFindings,
+      this.falseStatements
+        .map((statements) => statements.analyze(environment))
+        .unwrapOr(AnalysisFindings.empty()),
+    );
+    environment.analysisTable.popScope();
+    if (conditionFindings.isErroneous()) {
+      return findings;
+    }
+    const conditionType = this.condition.resolveType(environment);
+    if (!conditionType.isFundamental("Boolean")) {
+      findings.errors.push(AnalysisError(environment, {
+        message:
+          "The expression inside of the if statement needs to evaluate to a boolean value.",
+        beginHighlight: this.condition,
+        endHighlight: None(),
+      }));
+    }
+    return findings;
+  }
+
+  interpret(environment: ExecutionEnvironment): void {
+    environment.runtimeTable.pushScope();
+    const conditionResult = (this.condition as BooleanExpressionAstNode)
+      .evaluate(environment);
+    if (conditionResult.value) {
+      this.statements.interpret(environment);
+      environment.runtimeTable.popScope();
+    } else {
+      environment.runtimeTable.popScope();
+      environment.runtimeTable.pushScope();
+      this.falseStatements.then((statements) =>
+        statements.interpret(environment)
+      );
+      environment.runtimeTable.popScope();
+    }
+  }
+
+  get_representation(environment: ExecutionEnvironment): string {
+    this.interpret(environment);
+    return "Nothing";
+  }
+
+  tokenRange(): [Token<TokenKind>, Token<TokenKind>] {
+    return [this.keyword, this.closingBrace];
+  }
+}
+
+/* PARSER */
+
+loop.setPattern(
+  apply(
+    seq(
+      str<TokenKind>("while"),
+      kmid(
+        surround_with_breaking_whitespace(str("(")),
+        expression,
+        surround_with_breaking_whitespace(str(")")),
+      ),
+      kright(
+        ends_with_breaking_whitespace(str("{")),
+        statements,
+      ),
+      surround_with_breaking_whitespace(str("}")),
+    ),
+    ([keyword, condition, statements, closingBrace]) => {
+      return new LoopAstNode({
+        keyword,
+        condition,
+        statements,
+        closingBrace,
+      });
+    },
+  ),
+);
