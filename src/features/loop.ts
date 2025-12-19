327c0a8630a768c4bb792f8a44bd51102120a46b
LeonPuchinger <lepu1590@gmail.com>
2025-12-19 22:30:42 +0100

    make sure the scopes of conditions and loops are properly closed even when control flow modifiers or return values interrup the execution

--- src/features/loop.ts
+++ src/features/loop.ts
@@ -65,28 +65,39 @@ export class LoopAstNode implements InterpretableAstNode {
   interpret(environment: ExecutionEnvironment): void {
     environment.runtimeTable.pushScope();
     environment.typeTable.pushScope({ loop: true });
-    const conditionTrue = () =>
-      (this.condition as BooleanExpressionAstNode)
-        .evaluate(environment).value;
-    while (conditionTrue()) {
-      environment.runtimeTable.pushScope();
-      try {
-        this.statements.interpret(environment);
-      } catch (error) {
-        if (error instanceof ControlFlowModifier) {
-          if (error.modifier === "continue") {
-            continue;
+    try {
+      const conditionTrue = () =>
+        (this.condition as BooleanExpressionAstNode)
+          .evaluate(environment).value;
+      while (conditionTrue()) {
+        environment.runtimeTable.pushScope();
+        try {
+          this.statements.interpret(environment);
+        } catch (error) {
+          if (error instanceof ControlFlowModifier) {
+            if (error.modifier === "continue") {
+              continue;
+            }
+            if (error.modifier === "break") {
+              break;
+            }
           }
-          if (error.modifier === "break") {
-            break;
-          }
+          throw error;
+        } finally {
+          // Ensure that the scope is popped even if there was
+          // a control flow modifier or a function return value.
+          // Take a look at the implementation of conditions for
+          // furhter explanation.
+          environment.runtimeTable.popScope();
         }
-        throw error;
       }
+    } finally {
+      // Ensure that the loop scope is always popped.
+      // Take a look at the implementation of conditions for
+      // furhter explanation.
+      environment.typeTable.popScope();
       environment.runtimeTable.popScope();
     }
-    environment.typeTable.popScope();
-    environment.runtimeTable.popScope();
   }
 
   get_representation(environment: ExecutionEnvironment): string {
